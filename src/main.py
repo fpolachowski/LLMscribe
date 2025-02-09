5146214ac26ed7c4adc7a6594edd667ea6d35de7
Frederik Polachowski <frederik.polachowski@tu-dortmund.de>
2025-02-09 14:52:52 +0100

    expanded main with pdf loader

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from Types import Document
 from Client import OllamaClient
-from functions import generate_top_k_context, split_document
+from functions import generate_top_k_context, split_document, load_pdf
 
 client = OllamaClient(
     host="http://10.0.0.149:11434",
@@ -9,14 +9,7 @@ client = OllamaClient(
     template_path="src/templates/RAG_QUERY.txt",
 )
 
-knowledge = """
-Llamas are members of the camelid family meaning they're pretty closely related to vicuñas and camels.
-Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands.
-Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall.
-Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight.
-Llamas are vegetarians and have very efficient digestive systems.
-Llamas live to be about 20 years old, though some only live for 15 years and others live to be 30 years old.
-"""
+knowledge = load_pdf("test.pdf")
 
 question = "What animals are llamas related to?"
 
